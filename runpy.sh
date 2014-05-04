8be11e9a6069a55fa84fe8f9aa7ee1253ad9f3a0
pystone <pystone9205@gmail.com>
2014-05-04 16:53:27 +0800

    some personal test stuff

---
+++ runpy.sh
@@ -0,0 +1,6 @@
+#! /bin/bash
+gcc -c test.c
+
+ld -static -r /usr/lib/crt1.o /usr/lib/gcc/i686-redhat-linux/4.4.4/crtbegin.o -L/usr/lib -L/lib test.o -L/usr/lib/gcc/i686-redhat-linux/4.4.4 -lgcc -lgcc_eh -lc_nonshared /usr/lib/gcc/i686-redhat-linux/4.4.4/crtend.o /usr/lib/crtn.o  -o obj.o   
+
+./main obj.o /usr/lib/libc.so /lib/ld-2.12.so
---
+++ test.cc
@@ -0,0 +1,6 @@
+#include "SCLog.h"
+
+int main() {
+	SCLog(RL_ONE, "%d haha", 1);
+	return 0;
+}
